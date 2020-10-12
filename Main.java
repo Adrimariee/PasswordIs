b51b59e6ddb18ef453bf594354d964923e9aa7e8
Adrienne Davi <adrienne.davi@patelhs.org>
2020-10-12 13:53:22 +0000

    Added Two System.out.print statements!

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main {
   public static void main(String[] args) {
     
     System.out.println ("Hello!");
-    System.out.println ("Welcome to 'My Virtual Diary™'");
+    System.out.println ("Welcome to 'My Virtual Diary™'");\;
 
   }
 }
